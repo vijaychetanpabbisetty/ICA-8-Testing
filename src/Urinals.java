94090dd2b2024abfa9870a276114020d118e736b
smanda38 <smanda38@asu.edu>
2022-10-28 04:23:15 +0530

    Initial

---
+++ src/Urinals.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test;
+// @author vpabbise Vijaychetan Pabbisetty
+
+
+public class Urinals {
+
+
+    public static void main(String arg[]) {
+        getString();
+        openFile();
+        countUrinals();
+    }
+    public static void getString()
+    {
+        System.out.println("Not implemented");
+    }
+    public static void openFile()
+    {
+        System.out.println("Not implemented");
+    }
+
+    public static void countUrinals()
+    {
+        System.out.println("Not implemented");
+    }
+
+
+
+}
+
