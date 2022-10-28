bba0a876723151970d3cb5eb23b26294efd1edf2
smanda38 <smanda38@asu.edu>
2022-10-28 05:37:18 +0530

    Test cases

---
+++ src/urinals.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class urinals {
+    //create urinals.java class that works as a shell class
+
+    public static String in_str;
+    public static int urinalsAvailable(String urinals){
+        //available urinals "0's"
+        System.out.print("check if urinal is available");
+        return 0;
+    }
+    public void readFile(){
+        //Read the file
+    }
+    public String getString(){
+        //Get String from the file
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the keyboard input string:");
+        in_str = sc.next();
+        return in_str;
+
+    }
+    public void countUrinals(){
+        //Parse through the string to check free urinals
+    }
+    public void generateOutput(){
+        //Output a file that gives free urinals
+    }
+    Boolean goodString( String str ) { // checks to see if valid string
+        System.out.println("Not yet implemented");
+        return true;
+    }
+    public static void main(String[] args) {
+        System.out.println("main function");
+    }
+}
---
+++ src/urinalsTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class urinalsTest {
+    //Testcases
+
+
+    @Test
+    public void testpassfail() {
+        System.out.println("====== Vijaychetan == TEST ONE EXECUTED =======");
+       assertEquals(69,23);
+
+
+
+}
+}
